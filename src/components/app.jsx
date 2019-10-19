3d6bc4bdce63286e49535973aa0ba6943b12afd7
dzaitseva <dzaitseva@phoenixit.ru>
2019-10-19 20:50:53 +0300

    module2-task1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import MainPage from "./main-page/main-page.jsx";
 
-export const App = () => {
-  return <MainPage/>;
+const App = () => {
+  const films = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Spider Man`];
+  return <MainPage
+    films={films}
+  />;
 };
+
+export default App;
---
+++ src/components/catalog-card/catalog-card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CatalogCard = ({title}) => {
+
+  CatalogCard.propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
+  return (
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img
+          src="img/johnny-english.jpg"
+          alt={title}
+          width="280"
+          height="175" />
+      </div>
+      <h3 className="small-movie-card__title">
+        <a
+          className="small-movie-card__link"
+          href="movie-page.html">{title}</a>
+      </h3>
+    </article>
+  );
+};
+
+export default CatalogCard;
