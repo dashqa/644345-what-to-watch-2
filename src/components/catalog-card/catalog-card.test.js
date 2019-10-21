44875d70673bb2c7b8a52e85381d28fa02b35d81
dzaitseva <dzaitseva@phoenixit.ru>
2019-10-21 13:38:04 +0300

    update module2-task2

--- src/components/catalog-card/catalog-card.test.js
+++ src/components/catalog-card/catalog-card.test.js
@@ -6,7 +6,6 @@ it(`Catalog card correctly renders after relaunch`, () => {
   const tree = renderer
     .create(<CatalogCard
       title={`Spider Man`}
-      onTitleClick={jest.fn()}
     />)
     .toJSON();
   expect(tree).toMatchSnapshot();
