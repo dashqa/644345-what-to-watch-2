1bdb8d3c1bc68d6f499f1cc043fe1dc61ca9a197
dzaitseva <dzaitseva@phoenixit.ru>
2019-10-24 16:42:22 +0300

    module3-task1

--- src/components/catalog-card/catalog-card.jsx
+++ src/components/catalog-card/catalog-card.jsx
@@ -1,33 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const CatalogCard = ({title}) => {
-
-  const onTitleClick = (evt) => evt.preventDefault();
-
+const CatalogCard = ({movie, onCardHover}) => {
+  const {name, previewImage} = movie;
   return (
-    <article className="small-movie-card catalog__movies-card">
+    <article
+      className="small-movie-card catalog__movies-card"
+      onMouseEnter={() => onCardHover(movie)}
+    >
       <div className="small-movie-card__image">
         <img
-          src="img/johnny-english.jpg"
-          alt={title}
+          src={previewImage}
+          alt={name}
           width="280"
           height="175"/>
       </div>
-      <h3
-        className="small-movie-card__title"
-        onClick={onTitleClick}>
+      <h3 className="small-movie-card__title">
         <a
           className="small-movie-card__link"
-          href="movie-page.html">{title}</a>
+          href="movie-page.html">{name}</a>
       </h3>
     </article>
   );
 };
 
-CatalogCard.propTypes = {
-  title: PropTypes.string.isRequired,
-};
 
+CatalogCard.propTypes = {
+  movie: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    previewImage: PropTypes.string.isRequired
+  }).isRequired,
+  onCardHover: PropTypes.func.isRequired
+};
 
 export default CatalogCard;
--- src/components/catalog/catalog.jsx
+++ src/components/catalog/catalog.jsx
@@ -1,58 +1,59 @@
 import React from "react";
-import CatalogCard from "./../catalog-card/catalog-card.jsx";
 import PropTypes from "prop-types";
+import CatalogCard from "./../catalog-card/catalog-card.jsx";
+import {FILTERS} from "./../../constants";
 
-const Catalog = ({filmTitles}) => {
-  return (
-    <section className="catalog">
-      <h2 className="catalog__title visually-hidden">Catalog</h2>
+class Catalog extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-      <ul className="catalog__genres-list">
-        <li className="catalog__genres-item catalog__genres-item--active">
-          <a href="#" className="catalog__genres-link">All genres</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Comedies</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Crime</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Documentary</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Dramas</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Horror</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Kids & Family</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Romance</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Sci-Fi</a>
-        </li>
-        <li className="catalog__genres-item">
-          <a href="#" className="catalog__genres-link">Thrillers</a>
-        </li>
-      </ul>
+    this.state = {
+      activeCard: {},
+      activeFilter: `All genres`
+    };
 
-      <div className="catalog__movies-list">
-        {filmTitles.map((filmTitle, i) => <CatalogCard key={`${filmTitle}-${i}`} title={filmTitle}/>)}
-      </div>
+    this._handleCardHover = this._handleCardHover.bind(this);
+  }
 
-      <div className="catalog__more">
-        <button className="catalog__button" type="button">Show more</button>
-      </div>
-    </section>
-  );
-};
+  render() {
+    const {movies} = this.props;
+    return (
+      <section className="catalog">
+        <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+        <ul className="catalog__genres-list">
+          {FILTERS.map((filter, i) => (
+            <li
+              key={i}
+              className={`catalog__genres-item ` + (this.state.activeFilter === filter ? `catalog__genres-item--active` : ``)}>
+              <a href="#" className="catalog__genres-link">{filter}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="catalog__movies-list">
+          {movies.map((movie) =>
+            <CatalogCard
+              key={movie.id}
+              movie={movie}
+              onCardHover={this._handleCardHover}
+            />)}
+        </div>
+
+        <div className="catalog__more">
+          <button className="catalog__button" type="button">Show more</button>
+        </div>
+      </section>
+    );
+  }
+
+  _handleCardHover(movie) {
+    this.setState({activeCard: movie});
+  }
+}
 
 Catalog.propTypes = {
-  filmTitles: PropTypes.arrayOf(PropTypes.string),
+  movies: PropTypes.array.isRequired,
 };
 
 export default Catalog;
